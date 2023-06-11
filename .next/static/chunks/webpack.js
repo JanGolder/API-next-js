d2e3c4bc84ab81b210f2f5d9d687f286badae8cf
Jan Golder <kontakt.jangolder@gmail.com>
2023-06-11 13:43:37 +0200

    update styles

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8a0c584354e3d36e"; }
+/******/ 		__webpack_require__.h = function() { return "b1a096103650140d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
